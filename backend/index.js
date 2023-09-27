eabf9f111b7a982d9bc9b5f20150ce67961b498a
Brody Critchlow <24994624+brodycritchlow@users.noreply.github.com>
2023-09-27 17:53:40 +0000

    Use ESLINT to refactor `index.js`

--- backend/index.js
+++ backend/index.js
@@ -1,199 +1,221 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require('express')
-const app = express()
-const rateLimitMiddleware = require("./middlewares/ratelimit");
-const verifyToken = require("./middlewares/verify_token")
-const port = 5000
+// IMPORTS
 
+const express = require('express');
 const mysql = require('mysql');
 const session = require('express-session');
 const path = require('path');
-
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 const multer = require('multer');
 const cookieParser = require('cookie-parser');
 
+// MIDDLEWARES
 
+const rateLimitMiddleware = require('./middlewares/ratelimit');
+const verifyToken = require('./middlewares/verify_token');
+
+// CONSTANTS
+
+const app = express();
+const port = 5000;
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'uploads/'); // Specify the directory where uploaded files will be stored
-    },
-    filename: (req, file, cb) => {
-      const extname = path.extname(file.originalname);
-      cb(null, Date.now() + extname); // Rename the file with a unique name (timestamp)
-    },
-  });
-
-const upload = multer({ storage });
-
-// Handle POST requests for uploading images
-app.post('/upload', upload.single('photo'), (req, res) => {
-  if (!req.file) {
-    return res.status(400).send('No file uploaded.');
-  }
-
-  // Access the uploaded file details through req.file
-  const imageUrl = '/uploads/' + req.file.filename;
-
-  // Save imageUrl to your database or respond with it as needed
-  res.status(200).json({ imageUrl });
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    const extname = path.extname(file.originalname);
+    cb(null, Date.now() + extname);
+  },
 });
-  
-function generateAccessToken(pass) {
-    return jwt.sign(pass, process.env.JWT_KEY);
-}
-
-function createAccount(username, password, callback) {
-    connection.query('SELECT * FROM accounts WHERE username = ?', [username], function (error, results) {
-        if (error) {
-            callback(error);
-        } else {
-            if (results.length > 0) {
-                callback('Username already exists');
-            } else {
-                connection.query('INSERT INTO accounts (username, password) VALUES (?, ?)', [username, password], function (error) {
-                    if (error) {
-                        callback(error);
-                    } else {
-                        callback(null);
-                    }
-                });
-            }
-        }
-    });
-}
-
+const upload = multer({storage});
 const connection = mysql.createConnection({
-    host: '0.0.0.0',
-    user: 'root',
-    password: '',
-    database: 'nodelogin'
+  host: '0.0.0.0',
+  user: 'root',
+  password: '',
+  database: 'nodelogin',
 });
+
+// SETUP APP
 
 app.set('views', __dirname + '\\views');
 app.engine('html', require('ejs').renderFile);
 app.use(session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true,
 }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/static'));
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '\\views/login.html'));
+app.post('/upload', upload.single('photo'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+  const imageUrl = '/uploads/' + req.file.filename;
+  res.status(200).json({imageUrl});
 });
 
-app.post('/auth', function (request, response) {
-    let username = request.body.username;
-    let password = request.body.password;
+/**
+ * Generates a json web token, based on user's password.
+ * @constructor
+ * @param {string} pass - The users password
+ */
+function generateAccessToken(pass) {
+  return jwt.sign(pass, process.env.JWT_KEY);
+}
 
-    if (username && password) {
-        connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function (error, results, fields) {
-            if (error) throw error;
-            const tokenCookie = request.cookies._token;
-
-            if (tokenCookie) {
-                response.redirect('/home');
-            } else if (results.length > 0) {
-                request.session.username = username;
-
-                // Assuming you have a function to generate the JWT token
-                const token = generateAccessToken(username);
-                
-                // Set the JWT token as a cookie
-                response.cookie('_token', token, { expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), httpOnly: true });
-
-                response.redirect('/home');
-            } else {
-                createAccount(username, password, (err) => {
-                    if (err) {
-                        console.error('Error creating account:', err);
-                        response.status(500).send('Error creating account');
-                    } else {
-                        request.session.username = username;
-                        response.redirect('/home');
-                    }
-                });
-            }
-        });
-    } else {
-        response.send('Please enter Username and Password!');
-    }
-});
-
-app.get('/home', verifyToken, function (request, response) {
-    connection.query("SELECT * FROM createdposts", function (err, rows) {
-        if (err) {
-            console.error('Error retrieving entries:', err);
-            response.status(500).send('Error retrieving entries');
+/**
+ * Creates an account and adds it to the database.
+ * @constructor
+ * @param {string} username - The user's username.
+ * @param {string} password - The user's password.
+ * @param {Object} callback - The function that gets called on error.
+ */
+function createAccount(username, password, callback) {
+  connection.query(
+      'SELECT * FROM accounts WHERE username = ?',
+      [username], function(error, results) {
+        if (error) {
+          callback(error);
         } else {
-            response.render('index.ejs', { username: request.session.username, posts: rows });
+          if (results.length > 0) {
+            callback('Username already exists');
+          } else {
+            connection.query(
+                'INSERT INTO accounts (username, password) VALUES (?, ?)',
+                [username, password], function(error) {
+                  if (error) {
+                    callback(error);
+                  } else {
+                    callback(null);
+                  }
+                });
+          }
         }
-    });
+      });
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '\\views/login.html'));
+});
+
+app.post('/auth', function(request, response) {
+  const username = request.body.username;
+  const password = request.body.password;
+
+  if (username && password) {
+    connection.query(
+        'SELECT * FROM accounts WHERE username = ? AND password = ?',
+        [username, password], function(error, results, fields) {
+          if (error) throw error;
+          const tokenCookie = request.cookies._token;
+
+          if (tokenCookie) {
+            response.redirect('/home');
+          } else if (results.length > 0) {
+            request.session.username = username;
+
+            const token = generateAccessToken(username);
+
+            response.cookie(
+                '_token', token,
+                {expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+                  httpOnly: true},
+            );
+
+            response.redirect('/home');
+          } else {
+            createAccount(username, password, (err) => {
+              if (err) {
+                console.error('Error creating account:', err);
+                response.status(500).send('Error creating account');
+              } else {
+                request.session.username = username;
+                response.redirect('/home');
+              }
+            });
+          }
+        });
+  } else {
+    response.send('Please enter Username and Password!');
+  }
+});
+
+app.get('/home', verifyToken, function(request, response) {
+  connection.query('SELECT * FROM createdposts', function(err, rows) {
+    if (err) {
+      console.error('Error retrieving entries:', err);
+      response.status(500).send('Error retrieving entries');
+    } else {
+      response.render(
+          'index.ejs',
+          {username: request.session.username, posts: rows},
+      );
+    }
+  });
 });
 
 app.post('/createPost', rateLimitMiddleware, (req, res) => {
-    const postContent = req.body.postContent;
-    const user = req.session.username;
+  const postContent = req.body.postContent;
+  const user = req.session.username;
 
-    // Check if an image was uploaded
-    let imageUrl = '';
-    if (req.file) {
-        imageUrl = '/uploads/' + req.file.filename;
+  let imageUrl = '';
+  if (req.file) {
+    imageUrl = '/uploads/' + req.file.filename;
+  }
+
+  // eslint-disable-next-line max-len
+  const sql = 'INSERT INTO createdPosts (user, postcontent, imageUrl) VALUES (?, ?, ?)';
+  connection.query(sql, [user, postContent, imageUrl], (err, result) => {
+    if (err) {
+      console.error('Error creating a post:', err);
+      res.status(500).send('Error creating a post');
+      return;
     }
-
-    const sql = "INSERT INTO createdPosts (user, postcontent, imageUrl) VALUES (?, ?, ?)";
-    connection.query(sql, [user, postContent, imageUrl], (err, result) => {
-        if (err) {
-            console.error('Error creating a post:', err);
-            res.status(500).send('Error creating a post');
-            return;
-        }
-        console.log('Post created successfully');
-        res.send('Post created successfully');
-    });
+    console.log('Post created successfully');
+    res.send('Post created successfully');
+  });
 });
 
 
-app.route("/editPost")
-    .post(rateLimitMiddleware, function (req, res) {
-        const postID = req.body.postID;
-        const updatedPostContent = req.body.post;
+app.route('/editPost')
+    .post(rateLimitMiddleware, function(req, res) {
+      const postID = req.body.postID;
+      const updatedPostContent = req.body.post;
 
-        const sql = "UPDATE createdPosts SET postcontent = ? WHERE id = ?";
-        connection.query(sql, [updatedPostContent, postID], (err, result) => {
-            if (err) {
-                console.error('Error editing the post:', err);
-                res.status(500).send('Error editing the post');
-                return;
-            }
-            console.log('Post edited successfully');
-            res.send('Post edited successfully');
-        });
+      const sql = 'UPDATE createdPosts SET postcontent = ? WHERE id = ?';
+      connection.query(sql, [updatedPostContent, postID], (err, result) => {
+        if (err) {
+          console.error('Error editing the post:', err);
+          res.status(500).send('Error editing the post');
+          return;
+        }
+        console.log('Post edited successfully');
+        res.send('Post edited successfully');
+      });
     });
 
-app.route("/deletePost")
-    .post(rateLimitMiddleware, function (req, res) {
-        const postID = req.body.postID;
+app.route('/deletePost')
+    .post(rateLimitMiddleware, function(req, res) {
+      const postID = req.body.postID;
 
-        const sql = "DELETE FROM createdPosts WHERE id = ?";
-        connection.query(sql, [postID], (err, result) => {
-            if (err) {
-                console.error('Error deleting the post:', err);
-                res.status(500).send('Error deleting the post');
-                return;
-            }
-            console.log('Post deleted successfully');
-            res.send('Post deleted successfully');
-        });
+      const sql = 'DELETE FROM createdPosts WHERE id = ?';
+      connection.query(sql, [postID], (err, result) => {
+        if (err) {
+          console.error('Error deleting the post:', err);
+          res.status(500).send('Error deleting the post');
+          return;
+        }
+        console.log('Post deleted successfully');
+        res.send('Post deleted successfully');
+      });
     });
 
 
 app.listen(port, `0.0.0.0`, () => {
-    console.log(`Listening on 0.0.0.0:${port}`)
-})
+  console.log(`Listening on 0.0.0.0:${port}`);
+});
 
